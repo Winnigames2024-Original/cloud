99ed8c5370954fc8def28280e7b295ea5fad8c54
link-short <dora.family228@gmail.com>
2025-12-06 21:32:00 +0500

    Create script.js

---
+++ MallyKiller/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const heroHeading = document.querySelector('.hero h1');
+    heroHeading.style.color = 'blue'; // Example of DOM manipulation
+});
